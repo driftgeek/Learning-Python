d5b7770e9d4379cc2575f1f477c4c84b5880da4a
driftgeek <codingprogrammerhacker@gmail.com>
2016-10-05 18:48:17 -0400

    Add files via upload

--- VerifyPNT.py
+++ VerifyPNT.py
@@ -1,6 +1,3 @@
-import math     
-
-
 def VerifyPNT(N):
 
    if (N < 2): #The number 2 is considered as the first prime number. So 0 and 1 have to excluded from the range.
@@ -19,3 +16,56 @@ def VerifyPNT(N):
             return False
 
    return True
+
+
+
+
+
+piN = 0 #This is my pi(N)
+
+N1= 10
+N2= 100
+N3= 1000
+N4= 10000
+N5= 100000
+
+for i in range (1,N1):
+        if VerifyPNT(i) == True:
+##          print(i), ##I wanted to verify the prime numbers in the range from 1-10.
+          piN += 1
+tempN1 = piN/(math.log(N1))
+
+print [N1, piN, tempN1]
+
+for i in range (11,N2): # I excluded the previous range 1-10 from finding out the range from (1,100). Previously, I noticed I was getting an added number of prime numbers. It is suppose to be 25 but I was getting 29 instead. 
+        if VerifyPNT(i) == True:
+##            print (i),# I wanted to verify the prime numbers in the range from 1-100.
+            piN += 1
+tempN2 = piN/(math.log(N2))
+
+print [N2, piN, tempN2]
+
+for i in range (101,N3):
+        if VerifyPNT(i) == True:
+####            print(i), # Just verifying the prime numbers in the range.
+            piN += 1
+tempN3 = piN/(math.log(N3))
+
+print [N3, piN, tempN3]
+
+for i in range (1001, N4):
+        if VerifyPNT(i) == True:
+##            print(i), # Just verifying the prime numbers in the range.
+            piN += 1
+tempN4 = piN/(math.log(N4))
+
+print [N4, piN, tempN4]
+
+for i in range (10001, N5):
+        if VerifyPNT(i) == True:
+##            print(i), # Just verifying the prime numbers in the range.
+            piN += 1
+
+tempN5 = piN/(math.log(N5))            
+print  [N5, piN, tempN5]
+
